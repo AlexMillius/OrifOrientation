442d8bb2b97092649ec470e60ab65e167368891e
AlexMillius <alexmillius@gmail.com>
2016-12-16 08:53:14 +0100

    Initial Commit

---
+++ OrifOrientation/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  OrifOrientation
+//
+//  Created by Alex Millius on 16.12.16.
+//  Copyright © 2016 TumTum. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Test de connection à la base de donnée
+        var ref: FIRDatabaseReference!
+        ref = FIRDatabase.database().reference()
+        ref.child("users").setValue(["username": "test"])
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
