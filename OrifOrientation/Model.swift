da5026ec89708c70bfa0547a210870ab5be424c9
AlexMillius <alexmillius@gmail.com>
2016-12-16 09:07:32 +0100

    model avec les struct assuré et profession ok enum niveau professionel ok

---
+++ OrifOrientation/Model.swift
@@ -0,0 +1,31 @@
+//
+//  Model.swift
+//  OrifOrientation
+//
+//  Created by Mohamed Lee on 16.12.16.
+//  Copyright © 2016 TumTum. All rights reserved.
+//
+
+import Foundation
+
+struct Assuré {
+    let age:Int
+    let niveauProfInit:NivProf
+    let niveauProfAutorise:NivProf
+    let annéeExpDomaine:Int?
+}
+
+struct Profession {
+    let nom:String
+    let duréeFormation:Int //En année
+    let lien:NSURL
+}
+
+enum NivProf {
+    case aucun
+    case afp
+    case cfc
+    case brevet
+    case diplôme
+    case maitrise
+}
